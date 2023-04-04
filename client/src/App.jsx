3e2292639a47b50c71de74e8c313a9eec90421a0
yura-prots <yuraprots@gmail.com>
2023-04-04 18:45:51 +0300

    Canvas, Home, Customizer created and imported

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="head-text">
-      <h1>3D-Shop</h1>
-    </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
